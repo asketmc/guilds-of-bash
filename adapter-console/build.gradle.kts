d7df68c9318951cd0b1e89cf5556d13678b2e3a8
Asket <asketmc.team@gmail.com>
2026-01-21 22:51:30 +0100

    Add standalone JAR build workflow and configure ShadowJar plugin

--- adapter-console/build.gradle.kts
+++ adapter-console/build.gradle.kts
@@ -1,11 +1,11 @@
-// FILE: adapter-console/build.gradle.kts
-
 import org.gradle.api.tasks.JavaExec
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm")
     application
     id("org.jetbrains.kotlinx.kover")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     // detekt применяется и настраивается в root build.gradle.kts
 }
 
@@ -26,6 +26,14 @@ tasks.named<JavaExec>("run") {
     standardInput = System.`in`
     standardOutput = System.out
     errorOutput = System.err
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("all")
+    mergeServiceFiles()
+    manifest {
+        attributes["Main-Class"] = "console.MainKt"
+    }
 }
 
 // (detekt tasks configured in root)
